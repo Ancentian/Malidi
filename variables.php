d31f5a2dae8d9ae23b249182a5218a386cbe7dbc
Ancentian <ancentmbithi8@gmail.com>
2021-09-20 16:19:56 +0300

    Intro nd Variables

---
+++ variables.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+		variable - cointainer for storing values
+
+		process of defining variables
+
+		use $_My_Name
+
+		The assignment Sign that we use in assigning variables is =
+
+
+*/
+
+		$x = 20;
+		$y = 30;
+
+		$firstname = "Betta";
+		$lastname = "Class";
+
+		$fullname = $firstname ." ".$lastname;
+
+		$_volume = 5000;
+
+		echo $x;
+		
+
+
+?>
